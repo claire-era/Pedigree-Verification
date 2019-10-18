fdd30e1b88d59448f218fc1ce118a15477eabbaf
Claire Era <cuera@up.edu.ph>
2019-10-18 10:44:09 +0800

    added fishers method locally inside GetCrossType

--- src/pedigreeVerification/Step1.java
+++ src/pedigreeVerification/Step1.java
@@ -1,9 +1,13 @@
 package pedigreeVerification;
 
+import java.awt.FileDialog;
+import java.awt.Frame;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.rosuda.JRI.REXP;
+import org.rosuda.JRI.RMainLoopCallbacks;
 import org.rosuda.JRI.Rengine;
 
 import net.maizegenetics.dna.snp.GenotypeTable;
@@ -27,65 +31,83 @@ public class Step1 {
 		genos = ImportUtils.readFromHapmap(hmp_filename);
 		return genos;
 	}
-	
+
 	private static ArrayList<Integer> GetPolymorphicMarkers(GenotypeTable genos) {
 		ArrayList<Integer> polyMarkers = new ArrayList<>();
-		int hetCount,site = 0;
-		int numOfSites = genos.numberOfSites(); // for 0-indexing 
+		int hetCount, site = 0;
+		int numOfSites = genos.numberOfSites(); // for 0-indexing
 		int[] hcArr = new int[numOfSites];
-		
+
 		for (site = 0; site < numOfSites; site++) {
-				hetCount = genos.heterozygousCount(site);
-				hcArr[site] = hetCount;
+			hetCount = genos.heterozygousCount(site);
+			hcArr[site] = hetCount;
 		}
-		
-		//get indices of max count of polymorphism
+
+		// get indices of max count of polymorphism
 		int max = hcArr[0];
-		for(int i = 1; i < hcArr.length; i++) 
-			if(hcArr[i] > max)	max = hcArr[i];
-		
-		//find all indices of polymorphic markers matching the max value
-		for(site = 0; site < numOfSites; site++)
-			if(genos.isPolymorphic(site) && max == genos.heterozygousCount(site))
+		for (int i = 1; i < hcArr.length; i++)
+			if (hcArr[i] > max)
+				max = hcArr[i];
+
+		// find all indices of polymorphic markers matching the max value
+		for (site = 0; site < numOfSites; site++)
+			if (genos.isPolymorphic(site) && max == genos.heterozygousCount(site))
 				polyMarkers.add(site);
-		
-		
+
 		return polyMarkers; // returns index of the location of the polymorphic marker
 	}
-	
+
 	private static ArrayList<Integer> getInitSolution(ArrayList<Integer> polymarkers) {
 		ArrayList<Integer> taxa = new ArrayList<Integer>();
-		
-		int psite = polymarkers.get(0); //get the max site
-		for(int taxon = 0; taxon < genos.numberOfTaxa(); taxon++)
-			if(genos.isHeterozygous(taxon, psite))
-				taxa.add(taxon); //adds index of a sample included in the initial solution
-		
+
+		int psite = polymarkers.get(0); // get the max site
+		for (int taxon = 0; taxon < genos.numberOfTaxa(); taxon++)
+			if (genos.isHeterozygous(taxon, psite))
+				taxa.add(taxon); // adds index of a sample included in the initial solution
+
 		return taxa;
 	}
-	
-	private static Double getPvalue(double[] observed, double[] expected) {
-		Rengine re = new Rengine(null, false, null);
+
+	private static String createVector(double[] observed, double[] expected) {
+		// CREATE VECTOR OF (OBSERVED,EXPECTED) values
+		String res = "c(";
+		for (int a = 0; a < observed.length; a++)
+			res = res + Double.toString(observed[a]) + ",";
+
+		for (int a = 0; a < expected.length; a++) {
+			if (a == (expected.length - 1))
+				res = res + Double.toString(expected[a]) + ")";
+			else
+				res = res + Double.toString(expected[a]) + ",";
+		}
+		// -----------------
+		return res;
+	}
+
+	private static Double getPvalue(Rengine re, double[] observed, double[] expected) {
+		String res = createVector(observed, expected);
 		Double pval = null;
+		if (!re.waitForR()) {
+			System.out.println("Cannot load R");
+			return null;
+		}
 		try {
-			String sample_m = "matrix(c(3, 1, 1, 3), nrow = 2, dimnames = list(Guess = c(\"Milk\", \"Tea\"),Truth = c(\"Milk\", \"Tea\")))";
-			re.eval("TeaTasting=" + sample_m);
-			re.eval("result=fisher.test(TeaTasting, alternative = \"greater\")");
+			// ----- evaluate string
+			String sample_m = "matrix(" + res + ",nrow = 2)";
+			re.eval("observed=" + sample_m);
+			re.eval("result=fisher.test(observed, alternative = \"greater\")");
 			pval = re.eval("result$p.value").asDouble();
-//			System.out.println("P-value is = " + pval);
 		} catch (Exception e) {
 		}
-//		re.interrupt();
-		re.end();
-//		re.
-//		System.out.println(pval);
 		return pval;
 	}
 
 	private static ArrayList<Integer> GetCrossType(GenotypeTable genos) {
-		ArrayList<Integer> ct = new ArrayList<Integer>(); //CROSS TYPE / ALLELE COMBINATION after function
+		ArrayList<Integer> ct = new ArrayList<Integer>(); // CROSS TYPE / ALLELE COMBINATION after function
 		ArrayList<Integer> polymarkers = GetPolymorphicMarkers(genos);
-		ArrayList<Integer> initTaxaSol = getInitSolution(polymarkers); //generated initial solution after good marker selection //taxa indices of initial solution
+		ArrayList<Integer> initTaxaSol = getInitSolution(polymarkers); // generated initial solution after good marker
+																		// selection //taxa indices of initial solution
+		Rengine re = new Rengine(null, false, null);
 		int psite;
 		int nCountSites = genos.numberOfSites();
 		int pCountSites = polymarkers.size();
@@ -93,29 +115,35 @@ public class Step1 {
 		double p, q, X, Y, Z;
 		int N;
 //		double[] observed = new double[3];
-		double[] expected1 = { 0, initSolTaxa, 0 }; //ASSUMPTION: ALL SAMPLES ARE HET (Segregation: AA x aa) 100% het
-		double[] expected2 = { initSolTaxa/2, initSolTaxa/2, 0 }; //ASSUMPTION: HALF ARE HET AND HALF ARE HOMO DOMINANT (Segregation: AA x Aa) 50% self 50% het
+		double[] expected1 = { 0, initSolTaxa, 0 }; // ASSUMPTION: ALL SAMPLES ARE HET (Segregation: AA x aa) 100% het
+		double[] expected2 = { initSolTaxa / 2, initSolTaxa / 2, 0 }; // ASSUMPTION: HALF ARE HET AND HALF ARE HOMO
+																		// DOMINANT (Segregation: AA x Aa) 50% self 50%
+																		// het
 		double[][] result;
 		
+		
 //		for(int i = 0; i < initTaxaSol.size(); i++) {
-			for(int site = 0; site < nCountSites; site++) {
-				//use f-test for expected and observed values for each SNP only in the samples listed 
+		for (int site = 0; site < nCountSites; site++) {
+			// use f-test for expected and observed values for each SNP only in the samples
+			// listed
 //				psite = polymarkers.get(site);
-				p = genos.majorAlleleFrequency(site);
-				q = genos.minorAlleleFrequency(site);
-				N = genos.numberOfTaxa();
-				Y = genos.heterozygousCount(site); //HETEROZYGOUS
-				X = ((2 * N * p) - Y) / 2; //HOMOZYGOUS DOM
-				Z = ((2 * N * q) - Y) / 2; //HOMOZYGOUS REC
+			p = genos.majorAlleleFrequency(site);
+			q = genos.minorAlleleFrequency(site);
+			N = genos.numberOfTaxa();
+			Y = genos.heterozygousCount(site); // HETEROZYGOUS
+			X = ((2 * N * p) - Y) / 2; // HOMOZYGOUS DOM
+			Z = ((2 * N * q) - Y) / 2; // HOMOZYGOUS REC
 //				X = X / N;
 //				Z = Z / N;
 //				Y = Y / N;
-				
-				double[] observed = {X,Y,Z};
-				double pval_type1 = Step1.getPvalue(observed, expected1);
-				double pval_type2 = Step1.getPvalue(observed, expected2);
-				System.out.println(pval_type1 + " " + pval_type2);
-			}
+
+			double[] observed = { X, Y, Z };
+			double pval_type1 = Step1.getPvalue(re, observed, expected1);
+			System.out.println("thread " + pval_type1);
+
+			double pval_type2 = Step1.getPvalue(re, observed, expected2);
+			System.out.println(pval_type2 + " asd");
+		}
 //		}
 
 //		for (int i = 0; i < polymarkers.size(); i++) {
@@ -136,7 +164,7 @@ public class Step1 {
 //			observed[1] = Y;
 //			observed[2] = Z;
 //		}
-
+		re.end();
 		return null;
 	}
 
